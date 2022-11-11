b6de7d45140208ada6e053cbcebb666d5333b911
Rasmus_Dahlkvist <97rasdah@gmail.com>
2022-11-11 13:12:06 +0000

    Comment out imports, Use draw_graphics function for testing

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-import os
-from random import randint
+# import os
+# from random import randint
 import numpy as np
-from getkey import getkey, keys
+# from getkey import getkey, keys
 
 graphic_ocean = np.array([
     ["~", "~", "~", "~", "~"],
@@ -18,8 +18,12 @@ hidden_ocean = np.array([
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0]])
 
+
 def draw_graphics():
-    
+
     for y in range(5):
         for x in range(5):
             print(graphic_ocean[x, y], end=" ")
+        print("")
+
+draw_graphics()
