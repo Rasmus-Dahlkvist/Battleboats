9df3818a738e28ba010a9ad74fc90e6e70cf75e5
Rasmus_Dahlkvist <97rasdah@gmail.com>
2022-11-11 14:42:29 +0000

    Add logic for visible and invisible graphics in draw graphics function

--- run.py
+++ run.py
@@ -23,7 +23,23 @@ def draw_graphics():
 
     for y in range(5):
         for x in range(5):
+            if hidden_ocean[x, y] == 0:
+                graphic_ocean[x, y] = "~"
+                
+            elif hidden_ocean[x, y] == 1:
+                graphic_ocean[x, y] = "~" 
+                
+            elif hidden_ocean[x, y] == 2:
+                graphic_ocean[x, y] = "X" 
+                
+            elif hidden_ocean[x, y] == 3:
+                graphic_ocean[x, y] = "O" 
+
+    for y in range(5):
+        for x in range(5):
             print(graphic_ocean[x, y], end=" ")
         print("")
 
+
 draw_graphics()
+
